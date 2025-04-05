4f49c88c2d262db9634bccb9234f0ef745867c38
Sedna <seventesprit@gmail.com>
2025-04-05 11:48:01 +0200

    po 1 lekcji

--- Space Invaders/obj/Debug/net9.0/Space Invaders.AssemblyInfo.cs
+++ Space Invaders/obj/Debug/net9.0/Space Invaders.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Space Invaders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023b8180940d1528449c8b1d6d3d1e60db20efc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Space Invaders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Space Invaders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
